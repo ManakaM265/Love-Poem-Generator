b597b0575479c9af47c681e5cdb9ce2e59d783b0
Mahadi <manakamedia@gmail.com>
2025-05-07 21:51:56 +0200

    API integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
   new Typewriter("#generated-poem", {
-    strings: "Generating a poem for you, please wait...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "06a76be07c395ac5ec163f44afd921ot";
+  let prompt = `User instructions: Generate a love poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic AI assistant who loves to write short poems. Your mission is to write a four line poem and separate each line with a <br />.  Make sure to follow the user instructions. Sign the poem SheCodes AI inside a <strong> element at the end of the poem.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poem-generator-form");
