eb5489dad990c70de7cee784e9ec3c12467fdf0f
Mahadi <manakamedia@gmail.com>
2025-05-08 21:00:09 +0200

    UX: hid the poem display box
    The box that will display the poem will appear once a user clicks the button

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ function generatePoem(event) {
     "You are a romantic AI assistant who loves to write short poems. Your mission is to write a four line poem and separate each line with a <br />.  Make sure to follow the user instructions. Sign the poem SheCodes AI inside a <strong> element at the end of the poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#generated-poem");
+  poemElement.classList.remove("hidden");
+
   axios.get(apiUrl).then(displayPoem);
 }
 
